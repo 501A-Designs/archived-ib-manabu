62d20b8dfadaefde53f28dbc137269564b41321d
501A <art71903design.gmail.com>
2021-05-03 09:25:43 +0900

    fixed navbar

--- navbar.js
+++ navbar.js
@@ -1,44 +1,12 @@
-document.getElementById('menuButton').addEventListener('click', () => {
-  document.getElementById("menuContainer").style.display = "block";
-  document.getElementById("menuButton").setAttribute('id', 'closeMenuButton');
-});
-
-document.getElementById('closeMenuButton').addEventListener('click', () => {
-  document.getElementById("menuContainer").style.display = "none";
-  document.getElementById("closeMenuButton").setAttribute('id', 'menuButton');
-});
-
-
-
-// function writeNavbar() {
-//   window.document.write(`
-//   <button id="menuButton" onclick="menu()">+</button>
-//   <section id="menuContainer">
-//     <li>
-//       <button onclick="window.location.href='index.html';">ホーム</button>
-//     </li>
-//     <li>
-//       <button onclick="window.location.href='thinkingskills.html';">
-//         考えるスキル
-//       </button>
-//     </li>
-//     <li>
-//       <button onclick="window.location.href='selfmanagementskills.html';">
-//         自己管理スキル
-//       </button>
-//     </li>
-//     <li>
-//       <button onclick="window.location.href='researchskills.html';">
-//         リサーチスキル
-//       </button>
-//     </li>
-//     <li>
-//       <button onclick="window.location.href='educationpolicy.html';">
-//         教育理念
-//       </button>
-//     </li>
-//     <li>
-//       <button onclick="window.location.href='others.html';">その他</button>
-//     </li>
-//   </section>");
-// }
+function navEvent() {
+  const mC = document.getElementById("menuContainer");
+  const mB = document.getElementById('menuButton');
+  const cMB = document.getElementById('closeMenuButton');
+  if (mC.style.display === "block") {
+    cMB.setAttribute('id', 'menuButton');
+    mC.style.display = "none";
+  } else {
+    mB.setAttribute('id', 'closeMenuButton');
+    mC.style.display = "block";
+  }
+}
